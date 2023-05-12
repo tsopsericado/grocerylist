0aba5b7cfb8362b98ace83a482df15b1368f9c55
tsopsericado <ricardotsopse@gmail.com>
2023-05-12 15:16:29 +0100

    media querry problem resolution mainly for the mobile vue

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-$(function() {
-  var callback = function(event) {
-    event.preventDefault();
-    var input = $('input[type=text][name=item]'),
-
-    value = input.val(),
-    need = ($(event.target).attr('id')==='addNeed'),
-    item = $('<li><input type="checkbox" name="item">' + value + '<a href="#">remove</a></li>'),
-    list = (need) ? $('ul').first() : $('ul').last();
-
-    input.val("");
-    input.focus();
-
-    if (value ==="") return;
-
-    if (!need) {
-      item.find('input').attr('checked', true);
-    }
-    item.appendTo(list);
+const inputitem = document.querySelector("#inputItem");
+const form = document.querySelector(".form");
+const addButton = document.querySelector("#addbutton");
+const shoplist = document.querySelector(".forshopping");
+const clear = document.querySelector(".clear");
+// Event listener for the input button
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+  if(inputitem.value == "") {
+    alert("please add some text");
+  }else {
+    const newitem = createNewItem(inputitem.value);
+    shoplist.appendChild(newitem);
+    inputitem.value ="";
+    inputitem.focus();
+    clear.classList.remove("d-none");
   }
-  $('#addHave, #addNeed').click(callback);
-
-  $('ul').on('click', 'li a', function(event){
-        $(event.target).parent('li').remove();
-  });
-
-  $('ul').on('click', 'input[type=checkbox]', function(event){
-    var listItem = $(event.target).parent('li'),
-       list = (event.target.checked) ? $('ul').last() : $('ul').first();
-          listItem.appendTo(list);
-  });  
-
+// Event listener for the clear button 
+  clear.addEventListener("click", function(){
+    shoplist.innerHTML = "";
+  })
 });
+// This is the function that allows us to creat and append new items to our list.
+function createNewItem(inputValue) {
+  const item = document.createElement("li");
+  const span = document.createElement("span");
+  const delBtn = document.createElement("button");
+  const editBtn = document.createElement("button");
+  span.textContent = inputValue;
+  delBtn.textContent = "Delete"
+  editBtn.textContent = "Edit";
+  item.appendChild(span);
+  item.appendChild(delBtn);
+  item.appendChild(editBtn);
 
-
+  //Event listener for the delete button
+  delBtn.addEventListener("click", function(){
+    item.parentNode.removeChild(item);
+  });
+  //Event listener for the edit button 
+  editBtn.addEventListener("click", function(){
+    span.contentEditable = true;
+    span.focus();
+  });
+  return item
+}
